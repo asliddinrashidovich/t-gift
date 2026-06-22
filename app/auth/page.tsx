bebbb5ae63aa449d69fb08f81bf990ce0ed6fca7
Asliddin <asliddinrashidovich7@gmail.com>
2026-06-22 14:28:12 +0500

    safsadf

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: process.env.NEXT_PUBLIC_BASE_URL,
+        redirectTo: "https://t-gift-xy6f.vercel.app",
       },
     });
   };
